4b64b7404e629e00248d623643762cf65c522a52
Sergio <winksousa0@gmail.com>
2022-10-28 15:10:46 -0300

    signInPopup

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,11 +1,11 @@
-import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import GoogleButton from "react-google-button";
 import { auth } from "../firebase";
 
 export const SignIn = () => {
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
-    signInWithRedirect(auth, provider);
+    signInWithPopup(auth, provider);
   };
 
   return (
